528c6864acb3c8619af2471d56f21f250ee68a63
Coderature <uihoonpop@gmail.com>
2026-03-19 23:16:08 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,26 @@
 
 int main()
 {
+    int n, i;
+    int p = 1;
+
+    scanf("%d", &n);
+
+    if (n < 2)
+        p = 0;
+    else {
+        for (i = 2; i < n; i++) {
+            if (n % i == 0) {
+                p = 0;
+                break;
+            }
+        }
+    }
+
+    if (p == 1)
+        printf("true\n");
+    else
+        printf("false\n");
 
     return 0;
 }
-
