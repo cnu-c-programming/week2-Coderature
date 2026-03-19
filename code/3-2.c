ae42ddb70d156d78293b46c50bd737ef4973d232
Coderature <uihoonpop@gmail.com>
2026-03-19 23:06:13 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -2,7 +2,17 @@
 
 int main()
 {
+    int a = -1;
+    unsigned int b = 1;
+
+    // a < b를 비교, a가 자동 형변환
+    if (a < b) {
+        printf("a < b\n");
+    }
+    else {
+        // 결과적으로 매우 큰 양수와 1을 비교
+        printf("a >= b ???\n");
+    }
 
     return 0;
 }
-
