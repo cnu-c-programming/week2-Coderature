28e4e5500c12c56545190cb83d2247da10b8ef9e
Coderature <uihoonpop@gmail.com>
2026-03-19 23:08:00 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -2,7 +2,19 @@
 
 int main()
 {
+    int a, b, c;
+
+    // 대입
+    a = b = c = 5;
+    printf("%d %d %d\n", a, b, c);
+
+    // 우선순위
+    a = 2 + 3 * 4;
+    printf("%d %d %d\n", a, b, c);
+
+    // 증가 연산
+    c = a++ + ++b;
+    printf("%d %d %d\n", a, b, c);
 
     return 0;
 }
-
